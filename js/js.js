59f56c91c3f457aac29beb5cd00ef27cf7212db1
Lukerogan <126Lrogan@frhsd.com>
2024-11-20 12:29:02 -0500

    Second video

--- js/js.js
+++ js/js.js
@@ -1,1 +1,12 @@
 // first video went over using the console to view the document and how to get the elements in the javascript also how to store them in variables  
+// second video also shows us how to query the dom based on class names and tag names. 
+var titles = document.getElementsByClassName('title')
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)))
+
+
+Array.from(titles).forEach(function(item){
+    console.log(item);
+})
+
