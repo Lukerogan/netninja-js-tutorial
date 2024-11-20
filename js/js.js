80b38fea83951c62d2188d9d7d529092951d84b4
Lukerogan <126Lrogan@frhsd.com>
2024-11-20 12:12:18 -0500

    First video

--- js/js.js
+++ js/js.js
@@ -1,1 +1,1 @@
-// first video went over using the console to view the document and how to get the elements in the javascript also how to store them in variables 
+// first video went over using the console to view the document and how to get the elements in the javascript also how to store them in variables  
