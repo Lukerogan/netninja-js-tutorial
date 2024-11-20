44b9d4ea82991d045bc654cd4a065840dd91c7e9
Lukerogan <126Lrogan@frhsd.com>
2024-11-20 12:38:02 -0500

    third vid

--- js/js.js
+++ js/js.js
@@ -10,3 +10,16 @@ Array.from(titles).forEach(function(item){
     console.log(item);
 })
 
+// Third video 
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wmf);
+
+var books = document.querySelector('#book-list li .name');
+
+books = document.querySelectorAll('#book-list li .name');
+//  console.log(books);
+
+Array.from(books).forEach(function(book){
+    console.log(book)
+});
+
