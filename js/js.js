529af3432b7aaf209d94d663dc6d2ddba135cc16
Lukerogan <126Lrogan@frhsd.com>
2024-11-21 14:05:02 -0500

    9th vid

--- js/js.js
+++ js/js.js
@@ -79,3 +79,27 @@
 // console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
 // bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
+
+
+// // 9th vid
+// const listItems = document.querySelectorAll('#book-list ul li');
+
+// Array.from(listItems).forEach(function(item){
+//   item.addEventListener('click', (e) => {
+
+//     const li = e.target.parentElement;
+//     console.log('child element to remove:', li);
+//     console.log('parent element to remove child from:', li.parentElement);
+//     li.parentNode.removeChild(li);
+
+//   });
+// });
+
+// // prevent default behaviour
+
+// const link = document.querySelector('#page-banner a');
+
+// link.addEventListener('click', function(e){
+//   e.preventDefault();
+//   console.log('Navigation to', e.target.textContent, 'was prevented');
+// });
